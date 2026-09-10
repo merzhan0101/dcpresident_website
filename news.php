63bd6b30acfe1ab8e68014077d997d05abcc37a2
Merzhan Kuralay <merzhan0101@mail.ru>
2026-09-10 16:22:07 +0500

    update 10.09.2026

--- news.php
+++ news.php
@@ -37,7 +37,7 @@
               <span class="month"><?= getRussianMonth($newsDate->format('n')) ?></span>
             </div>
             <div class="news-image-page">
-              <img src="<?= $newsItem['image_path'] ?: 'images/news.jpg' ?>" 
+              <img src="<?= $newsItem['image_path'] ?: 'images/news.png' ?>" 
                    alt="<?= htmlspecialchars($newsItem['title']) ?>">
             </div>
             <div class="news-content-page">
